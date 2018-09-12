57bde08b3a3578d5d0e7596f7735a162387b5173
Alan <alan88w@yahoo.com>
2018-09-12 22:28:13 +0800

    change localhost to https://vast-shore-64670.herokuapp.com

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -25,7 +25,7 @@ class Register extends React.Component {
 
     onRegister = (event) => {
 
-        fetch('http://localhost:3000/register', {
+        fetch('https://vast-shore-64670.herokuapp.com/register', {
             method: 'post',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
--- src/components/Signin/SignIn.js
+++ src/components/Signin/SignIn.js
@@ -21,7 +21,7 @@ class SignIn extends React.Component {
 
     onSubmitSignIn = (event) => {
 
-        fetch('http://localhost:3000/signin', {
+        fetch('https://vast-shore-64670.herokuapp.com/signin', {
             method: 'post',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
